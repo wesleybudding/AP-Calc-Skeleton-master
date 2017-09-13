b24448e06148b58378d80e4b884fa098c173267a
wesleybudding <wesleybudding@gmail.com>
2017-09-13 15:28:27 +0200

    TokenList

--- src/main/java/TokenList.java
+++ src/main/java/TokenList.java
@@ -1,5 +1,24 @@
 /**
  * Created by wesleybudding on 13/09/2017.
  */
-public class TokenList  {
+public class TokenList implements TokenList_interface {
+    public void add(Token token) {
+        
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public void set(int index, Token token) {
+
+    }
+
+    public Token get(int index) {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
 }
